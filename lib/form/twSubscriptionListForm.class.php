60e58dff96247adadb1f8c1b284c72445ca29754
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2015-02-16 22:32:07 +0100

    widget and validator

--- lib/form/twSubscriptionListForm.class.php
+++ lib/form/twSubscriptionListForm.class.php
@@ -10,6 +10,8 @@ class twSubscriptionListForm extends BasetwSubscriptionListForm
 {
     public function configure()
     {
+        $this->setWidget('smtp_encr', new sfWidgetFormChoice(array('choices' => array(0 => 'Bez szyfrowania', 2 => 'STARTTLS', 1 => 'SSL/TLS'))));
+        $this->setValidator('smt_encr', new sfValidatorChoice(array('required' => false, 'choices' => array(0, 1, 0))));
         $this->setValidator('from_address', new sfValidatorEmail());
         unset($this['language_id']);
         unset($this['last_sync_at']);
