bbbdbf44dff4170d1cac39b9e04ef92a4977285f
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2015-11-28 18:10:32 +0100

    delete deprecated functionality

--- modules/twSubscriptionUnsubscribe/actions/actions.class.php
+++ modules/twSubscriptionUnsubscribe/actions/actions.class.php
@@ -13,25 +13,7 @@ class twSubscriptionUnsubscribeActions extends sfActions
     {
         $list_id = $request->getParameter('id');
         $auth_key = $request->getParameter('auth_key');
-        $cmd = $request->getParameter('cmd');
 
         $this->email = twSubscriptionEmailQuery::create()->unsubscribe($auth_key, $list_id);
-
-        $this->content = false;
-
-        // TODO: twBasicCmsPlugin is deprecated
-        if (in_array('twBasicCmsPlugin', $this->getContext()->getConfiguration()->getPlugins()) && is_null($cmd)) {
-            $template = twBasicCmsTemplatePeer::doSelectForCode('core.subscription.unsubscribe');
-            if ($template instanceof twBasicCmsTemplate) {
-                sfContext::getInstance()->getConfiguration()->loadHelpers(array(
-                    'Partial'
-                ));
-                $generator = new twBasicCmsGenerator($this);
-                $content = get_partial('index', array(
-                    'email' => $this->email
-                ));
-                $this->content = $generator->generate($template, $content);
-            }
-        }
     }
 }
